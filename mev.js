76f670bb71b7b638a698a7e1a3f57c5f9a6f66aa
xanxys <xanxys@gmail.com>
2019-04-10 20:26:29 +0900

    Init three.js & html / add circular stage

---
+++ mev.js
@@ -0,0 +1,34 @@
+
+class MevApplication {
+    constructor(width, height, canvasInsertionParent) {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, width / height, 0.01, 50);
+        this.camera.position.set(-3, 1, 0);
+        this.camera.lookAt(0, 0.9, 0);
+
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(width, height);
+        this.renderer.antialias = true;
+        canvasInsertionParent.appendChild(this.renderer.domElement);
+
+        this.renderer.setClearColor(new THREE.Color("#ddd"));
+
+        const stageGeom = new THREE.CircleBufferGeometry(1, 64);
+        const stageMat = new THREE.MeshBasicMaterial({ color: "white" });
+        const stageObj = new THREE.Mesh(stageGeom, stageMat);
+        stageObj.setRotationFromAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI * 1.5);
+        this.scene.add(stageObj);
+    }
+
+    animate() {
+        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(() => this.animate);
+    }
+}
+
+function main() {
+    const app = new MevApplication(window.innerWidth, window.innerHeight, document.body);
+    app.animate();
+}
+
+main();
