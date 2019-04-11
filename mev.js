b8b35a877a1e618f90470665e4bdad9b7526707d
xanxys <xanxys@gmail.com>
2019-04-11 21:57:49 +0900

    Make scene rotatable & display textures

--- mev.js
+++ mev.js
@@ -1,11 +1,14 @@
+"use strict";
 
 class MevApplication {
     constructor(width, height, canvasInsertionParent) {
         // Three.js canvas
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, width / height, 0.01, 50);
-        this.camera.position.set(-3, 1, 0);
+        this.camera.position.set(0, 1, -3);
         this.camera.lookAt(0, 0.9, 0);
+
+        this.controls = new THREE.OrbitControls(this.camera);
 
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(width, height);
@@ -26,6 +29,8 @@ class MevApplication {
         const scene = this.scene;
         const vm = new Vue({
             el: '#vue_menu',
+            data: {
+            },
             methods: {
                 change_file: function (event) {
                     console.log(event.srcElement.files[0]);
@@ -40,6 +45,13 @@ class MevApplication {
                             vrm => {
                                 console.log("VRM loaded", vrm);
                                 scene.add(vrm.model);
+
+                                console.log(vrm.textures);
+
+                                vrm.textures.filter(e => e !== undefined).forEach(e => {
+                                    e.image.width = "200";
+                                    document.getElementById("textures").appendChild(e.image);
+                                });
                             },
                             progress => {
                             },
@@ -54,7 +66,9 @@ class MevApplication {
     }
 
     animate() {
+        this.controls.update();
         this.renderer.render(this.scene, this.camera);
+
         requestAnimationFrame(() => this.animate());
     }
 }
