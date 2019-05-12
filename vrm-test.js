6a1c16062d087b9100b7b3d961790e3a2691bd12
xanxys <xanxys@gmail.com>
2019-05-12 19:00:55 +0900

    Add serialize -> deserialize -> serialize identity test (currently failing)

--- vrm-test.js
+++ vrm-test.js
@@ -1,6 +1,23 @@
 // ES6
 import { deserializeVrm, serializeVrm } from './vrm.js';
 
+/**
+ * 
+ * @param {THREE.Object3D} obj 
+ */
+function summarizeObject(obj) {
+    const stats = {
+        numObjects: 0,
+        perTypeNumObjects: {},
+    };
+    obj.traverse(o => {
+        stats.numObjects++;
+        stats.perTypeNumObjects[o.type] = (stats.perTypeNumObjects[o.type] || 0) + 1;
+    });
+    return stats;
+}
+
+// TODO: Consider using https://qunitjs.com/
 class VrmIdentityTester {
     constructor() {
         this.testDataRepository = "http://localhost:8080";
@@ -13,25 +30,36 @@ class VrmIdentityTester {
                 testDataIndex.vrm.forEach(file => {
                     const vrmUrl = this.testDataRepository + "/" + file;
 
-                    window.fetch(vrmUrl, { mode: "cors" }).then(response => response.arrayBuffer()).then(vrmBlob => {
-                        console.log(vrmBlob);
-                        deserializeVrm(vrmBlob).then(vrm => {
-                            const res = document.createElement("div");
-                            const resMessage = document.createTextNode(file + " " + "OK");
-                            res.appendChild(resMessage);
-                            document.body.appendChild(res);
+                    window.fetch(vrmUrl, { mode: "cors" }).then(response => response.arrayBuffer())
+                        .then(deserializeVrm)
+                        .then(origVrm => {
+                            serializeVrm(origVrm)
+                                .then(deserializeVrm)
+                                .then(sdVrm => {
+                                    // TODO: compare
+                                    const origSummary = summarizeObject(origVrm);
+                                    const sdSummary = summarizeObject(sdVrm);
+
+                                    if (JSON.stringify(origSummary) !== JSON.stringify(sdSummary)) {
+                                        this.report(file + " " + "NG" + JSON.stringify(origSummary) + "  " + JSON.stringify(sdSummary));
+                                    } else {
+                                        this.report(file + " " + "OK");
+                                    }
+                                })
+                                .catch(err => {
+                                    this.report(file + " " + "NG" + err);
+                                });
+
                         });
-                    });
                 });
             });
+    }
 
-        // loadVrm();
-
-        /*
-        const vrmRoot  =     serializeVrm(app.vrmRoot).then(glbBuffer => {
-            saveAs(new Blob([glbBuffer], { type: "application/octet-stream" }), "test.vrm");
-        });
-        */
+    report(message) {
+        const res = document.createElement("div");
+        const resMessage = document.createTextNode(message);
+        res.appendChild(resMessage);
+        document.body.appendChild(res);
     }
 }
 
