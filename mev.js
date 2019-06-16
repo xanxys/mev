1e4e34dfb2de819d545c77a9540f9e8a730ba9db
xanxys <xanxys@gmail.com>
2019-06-16 20:44:24 +0900

    Almost OK animation playback

--- mev.js
+++ mev.js
@@ -134,7 +134,11 @@ class MevApplication {
         const app = this;
         app.heightIndicator = new HeightIndicator(this.scene);
 
-        const req = new Request("https://s3.amazonaws.com/open-motion.herokuapp.com/json/01_01.json");
+        // ID: http://mocap.cs.cmu.edu/search.php?subjectnumber=%&motion=%
+        // 01_09: good complex 3D movement
+        // 09_12: walk in various direction
+        // 40_10: "wait for bus" movement (good for idle)
+        const req = new Request("https://s3.amazonaws.com/open-motion.herokuapp.com/json/40_10.json");
         app.motionFrames = [];
         app.motionFrameIndex = 0;
         fetch(req).then(response => response.json()).then(json => {
@@ -435,7 +439,7 @@ class MevApplication {
         if (this.motionFrameIndex === 100 && this.motionFrames.length > 0) {
             console.log("Motion frame example:", currentMotion);
         }
-        this.motionFrameIndex += 1;
+        this.motionFrameIndex += 2;
 
         // TODO: apply
         if (this.vm.vrmRoot && this.vrmRenderer) {
@@ -445,7 +449,7 @@ class MevApplication {
             const lrMapping = new Map([
                 ["clavicle", "Shoulder"], // almost 0
                 ["humerus", "UpperArm"],
-                ["radius", "LowerArm"],
+                ["radius", "LowerArm"], // {rx}
                 ["wrist", "Hand"],
                 ["femur", "UpperLeg"],
                 ["tibia", "LowerLeg"],
@@ -470,7 +474,7 @@ class MevApplication {
 
 
             // Coordinate Systems:
-            // VRM/three: 
+            // VRM/three: https://gyazo.com/19731bf972cdd0fee866ee03d8634785
             // ASF/AMC: https://gyazo.com/6e8f786af0028d6effcb6bb77202b428
 
             if (inst !== null) {
@@ -478,28 +482,58 @@ class MevApplication {
                     const nodeIndex = vrmNameToNodeIndex.get(boneName);
                     const bone = this.vrmRenderer.getNodeByIndex(nodeIndex);
                     const val = currentMotion[asfName];
+                    if (!bone) {
+                        return;
+                    }
                     if (!bone || !val) {
                         console.log("Not found", boneName, bone, asfName, val);
                         return;
                     }
+
                     if (asfName === "root") {
                         bone.position.set(-val.tx, val.ty, -val.tz);
                     }
 
-                    // TODO: Need to rotate some bones with 45-degree rotation.
-                    if (asfName.includes("humerus")) {
-                        bone.quaternion.setFromEuler(new THREE.Euler(val.rx, val.ry, val.rz || 0, "ZYX"));
-                    } else if (asfName.includes("lradius")) {
+                    if (boneName.includes("UpperArm")) {
+                        const xz = new THREE.Vector2(val.rx, val.rz);
+
+                        const clavicleRot = 0.40; // from sekelton.rclavicle (XY)
+                        const isq2 = 1 / Math.sqrt(2);
+
+                        if (boneName === "rightUpperArm") {
+                            const qX = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, isq2, isq2), val.rx);
+                            const qY = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(-1, 0, 0), val.ry);
+                            const qZ = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, -isq2, isq2), val.rz);
+                            const boneDeltaRot = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 0, 1), clavicleRot);
+    
+                            qZ.multiply(qY);
+                            qZ.multiply(qX);
+                            qZ.multiply(boneDeltaRot);
+    
+                            bone.quaternion.copy(qZ);
+                        } else {
+                            const qX = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, -isq2, -isq2), val.rx);
+                            const qY = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), val.ry);
+                            const qZ = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, -isq2, isq2), val.rz);
+                            const boneDeltaRot = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 0, 1), -clavicleRot);
+    
+                            qZ.multiply(qY);
+                            qZ.multiply(qX);
+                            qZ.multiply(boneDeltaRot);
+    
+                            bone.quaternion.copy(qZ);
+                        }
+                    } else if (boneName.includes("leftLowerArm")) {
                         bone.quaternion.setFromEuler(new THREE.Euler(0, -val.rx, 0, "ZYX"));
-                    } else if (asfName.includes("rradius")) {
+                    } else if (boneName.includes("rightLowerArm")) {
                         bone.quaternion.setFromEuler(new THREE.Euler(0, val.rx, 0, "ZYX"));
                     } else if (boneName.includes("UpperLeg")) {
                         const legRotationZ = 0.350; // from skeleton.lfemur
                         bone.quaternion.multiplyQuaternions(
                             new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 0, 1), boneName === "leftUpperLeg" ? -legRotationZ : legRotationZ),
                             new THREE.Quaternion().setFromEuler(new THREE.Euler(-val.rx, val.ry, -val.rz, "ZYX"))
-                            );
-                    } else if (asfName.includes("foot") || asfName.includes("toes")) {
+                        );
+                    } else if (boneName.includes("Foot") || boneName.includes("Toes")) {
                         bone.quaternion.setFromEuler(new THREE.Euler(-val.rx, val.rz, val.ry, "ZYX"));
                     } else {
                         bone.quaternion.setFromEuler(new THREE.Euler(-val.rx || 0, val.ry || 0, -val.rz || 0, "ZYX"));
@@ -558,7 +592,7 @@ class MevApplication {
 
                         instance.traverse(o => {
                             if (o.type === "Bone") {
-                                o.add(new THREE.AxesHelper(0.3));
+                                //o.add(new THREE.AxesHelper(0.3));
                             }
 
                         });
