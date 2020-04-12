11cfe6ce71eb961d69264d4ad07c861a9f88ae85
xyx <xanxys@gmail.com>
2020-04-12 16:26:25 +0900

    Morph reduction code

--- vrm-reducer.js
+++ vrm-reducer.js
@@ -17,7 +17,7 @@ export async function reduceVrm(model) {
     //// misc
     // float-quantization
 
-    await removeAllBlendshapes(model);
+    // await removeAllBlendshapes(model);
     await removeUnusedMorphs(model);
     await extremeResizeTexture(model, 128);
     return null;
@@ -43,10 +43,84 @@ async function extremeResizeTexture(model, maxTexSizePx) {
  * @returns {Promise<null>}
  */
 async function removeAllBlendshapes(model) {
+    model.gltf.extensions.VRM.blendShapeMaster.blendShapeGroups = [];
+    model.version += 1;
 }
 
 /**
  * @returns {Promise<null>}
  */
 async function removeUnusedMorphs(model) {
+    if (!isUniformPrimitive(model)) {
+        console.warn("A mesh in the VRM has multiple primitives with different morphs. removeUnusedMorphs won't be executed.");
+        return;
+    }
+
+    // Extract pairs to keep
+    const usedMeshIdMorphIdPairs = new Set(); // meshId:morphId format.
+    model.gltf.extensions.VRM.blendShapeMaster.blendShapeGroups.forEach(group => {
+        group.binds.forEach(bind => {
+            const mesh = model.gltf.meshes[bind.mesh];
+            usedMeshIdMorphIdPairs.add(`${bind.mesh}:${bind.index}`);
+        });
+    });
+
+    // Execute deletion, with delta
+    const morphRemap = new Map(); // key=meshId:morphId value=newMorphId
+    model.gltf.meshes.forEach((mesh, meshId) => {
+        if (mesh.primitives.length === 0 || !mesh.primitives[0].targets) {
+            return;
+        }
+
+        const numMorphs = mesh.primitives[0].targets.length;
+        const newTargets = mesh.primitives.map(_ => []); // for each primitive
+        const newExtraTargetNames = mesh.primitives.map(_ => []); // for each primitive
+
+        let newMorphIx = 0;
+        for (let morphIx = 0; morphIx < numMorphs; morphIx++) {
+            const key = `${meshId}:${morphIx}`;
+            if (usedMeshIdMorphIdPairs.has(key)) {
+                morphRemap.set(key, newMorphIx);
+                mesh.primitives.forEach((prim, primIx) => {
+                    newTargets[primIx].push(prim.targets[morphIx]);
+                    newExtraTargetNames.push(
+                        (prim.extras && prim.extras.targetNames) ? prim.extras.targetNames[morphIx] : "");
+                });
+                newMorphIx++;
+            }
+        }
+
+        mesh.primitives.forEach((prim, primIx) => {
+            prim.targets = newTargets[primIx];
+            if (prim.extras && prim.extras.targetNames) {
+                prim.extras.targetNames = newExtraTargetNames;
+            }
+        });        
+    });
+    console.log("Morph remap", morphRemap);
+
+    // Apply delta to blendshapes.
+    model.gltf.extensions.VRM.blendShapeMaster.blendShapeGroups.forEach(group => {
+        group.binds.forEach(bind => {
+            const key = `${bind.mesh}:${bind.index}`;
+            console.assert(morphRemap.has(key));
+            bind.index = morphRemap.get(key);
+        });
+    });
+}
+
+function isUniformPrimitive(model) {
+    return model.gltf.meshes.every(mesh => {
+        if (mesh.primitives.length === 0) {
+            return true;
+        }
+
+        const refPrim = mesh.primitives[0];
+        return mesh.primitives.every(prim => {
+            if (refPrim.targets === undefined) {
+                return prim.targets === undefined;
+            }
+            return prim.targets.length === refPrim.targets.length;
+        });
+    });
 }
